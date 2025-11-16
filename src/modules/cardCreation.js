96923bb7a760d146c0067fdd2d4f464165c1c48f
Egor <tihonovegor09@gmail.com>
2025-11-16 23:28:17 +0300

    added carousel

--- src/modules/cardCreation.js
+++ src/modules/cardCreation.js
@@ -1,37 +1,81 @@
-import '../style.css'
-import axios from 'axios'
+import "../style.css";
+import axios from "axios";
 
-const findGit = document.getElementsByName('findGit')[0]
-const githubRe = /^^([a-z0-9A-Z.-]+)\/([a-z0-9A-Z.-]+)$/
-let cardAmount = document.getElementsByName('card')
+const findGit = document.getElementsByName("findGit")[0];
+const githubRe = /^^([a-z0-9A-Z.-]+)\/([a-z0-9A-Z.-]+)$/;
+let cardAmount = document.getElementsByName("card");
+let cardIteration = 0;
 
-export function createCard(e,userLogger){
-  userLogger.innerText = ''
-  const cardHolder = document.getElementsByName('cardHolder')[0]
-  const gitString = e.target.childNodes[1].childNodes[3].value.split(' ')
-  const regexCheckArr = new Map(gitString.map((element)=>{
-    let res = githubRe.exec(element)
-    if(res){
+export function createCard(e, userLogger) {
+  userLogger.innerText = "";
+  const gitString = e.target.childNodes[1].childNodes[3].value.split(" ");
+  const regexCheckArr = new Map(
+    gitString
+      .map((element) => {
+        let res = githubRe.exec(element);
+        if (res) {
+          return [res[1], res[2]];
+        }
+        userLogger.innerText += `Неверный ввод данных!(${element})\n`;
+        return undefined;
+      })
+      .filter(Boolean)
+  );
 
-      return [res[1], res[2]]
-    }
-    userLogger.innerText+=`Неверный ввод данных!(${element})\n`
-    return undefined
-  })
-.filter(Boolean))
+  //console.log(regexCheckArr);
 
-console.log(regexCheckArr)
+  regexCheckArr.forEach((name, owner, map) => {
+    axios
+      .get(`https://api.github.com/repos/${owner}/${name}`)
+      .then((githubInfo) => {
+        console.log(githubInfo);
+        cardAmount = document.getElementsByName("card").length;
 
-regexCheckArr.forEach((name, owner, map)=>{
-    axios.get(`https://api.github.com/repos/${owner}/${name}`)
-    .then((githubInfo)=>{
-      console.log(githubInfo)
-      cardAmount = document.getElementsByName('card');
-      if(cardAmount%3===0 && cardAmount!=0){
-        
-      }
-    const html = `
-        <div class="card col-4" name="card">
+        if (cardAmount % 3 === 0 && cardAmount != 0) {
+          cardIteration++;
+          const carouselHolder =
+            document.getElementsByClassName("carousel-inner")[0];
+          console.log(carouselHolder);
+          const carouselItemNew = document.createElement("div");
+          carouselItemNew.className += "carousel-item container-fluid";
+          const cardHolderNew = document.createElement("div");
+          cardHolderNew.className += "row";
+          cardHolderNew.setAttribute("name", `cardHolder-${cardIteration}`);
+          carouselHolder.appendChild(carouselItemNew);
+          carouselItemNew.appendChild(cardHolderNew);
+          console.log(carouselHolder);
+
+          const indicatorHolder = document.getElementsByClassName(
+            "carousel-indicators"
+          )[0];
+
+          const newIndicator = document.createElement("button");
+          newIndicator.setAttribute("type", "button");
+          newIndicator.setAttribute(
+            "data-bs-target",
+            "#carouselExampleIndicators"
+          ); 
+          newIndicator.setAttribute(
+            "data-bs-slide-to",
+            cardIteration.toString()
+          );
+          newIndicator.setAttribute("aria-label", `Slide ${cardIteration + 1}`);
+
+          indicatorHolder.appendChild(newIndicator);
+
+          const myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleIndicators'));
+        }
+
+        const cardHolder = document.getElementsByName(
+          `cardHolder-${cardIteration}`
+        )[0];
+        console.log(
+          cardIteration,
+          document.getElementsByName(`cardHolder-${cardIteration}`)[0]
+        );
+
+        const card = `
+        <div class="card col-4 p-3" name="card">
           <div class="card-body">
             <a href='${githubInfo.data.git_url}'>${owner}/${name}</a>
             <span>Звёзд: ${githubInfo.data.stargazers_count}</span>
@@ -47,13 +91,13 @@ regexCheckArr.forEach((name, owner, map)=>{
             </p>
           </div>
         </div>
-          `
+          `;
 
-  cardHolder.innerHTML += html
-})
-.catch((err)=>{
-      console.log(err)
-      userLogger.innerText += `${owner}/${name} не найден!\n`
-    })
-})
+        cardHolder.innerHTML += card;
+      })
+      .catch((err) => {
+        console.log(err);
+        userLogger.innerText += `${owner}/${name} не найден!\n`;
+      });
+  });
 }
